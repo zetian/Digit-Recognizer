efc4d1a93a9c67462530e13da5c35999ac4974b3
Zetian <zetianzzt@gmail.com>
2017-11-03 14:48:59 -0400

    cleanning

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Digit-Recognizer
+
+Code for Python digit recognizer starter comptetition. http://www.kaggle.com/c/digit-recognizer
+
+Use convolutional neural network model which attains a validation accuracy of about 98.66% is obtained after training for 10 epochs.
+
+## Requirements
+Python2.7
+pytorch
+numpy
+pandas
+matplotlib
--- digit_recognizer.py
+++ digit_recognizer.py
@@ -18,7 +18,6 @@ EPOCH = 20
 BATCH_SIZE = 50
 LR = 0.001          
 
-
 class DigitData(data.Dataset):
     # img_to_tensor = transforms.Compose([transforms.ToTensor()])
     img_to_tensor = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.1307,), (0.3081,))])
@@ -45,7 +44,6 @@ class DigitData(data.Dataset):
         plt.title(label)
         plt.show()
 
-
 class TestData(data.Dataset):
     # img_to_tensor = transforms.Compose([transforms.ToTensor()])
     img_to_tensor = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.1307,), (0.3081,))])
@@ -62,8 +60,6 @@ class TestData(data.Dataset):
         return image
     def __len__(self):
         return self.images.shape[0]
-
-
 
 class CNN(nn.Module):
     def __init__(self):
@@ -83,32 +79,12 @@ class CNN(nn.Module):
         x = self.fc2(x)
         return F.log_softmax(x)
 
-
 # optimizer = optim.SGD(cnn_net.parameters())
-
-# def train(epoch):
-#     model.train()
-#     for batch_idx, (data, target) in enumerate(train_loader):
-#         if args.cuda:
-#             data, target = data.cuda(), target.cuda()
-#         data, target = Variable(data), Variable(target)
-#         optimizer.zero_grad()
-#         output = model(data)
-#         loss = F.nll_loss(output, target)
-#         loss.backward()
-#         optimizer.step()
-#         if batch_idx % args.log_interval == 0:
-#             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-#                 epoch, batch_idx * len(data), len(train_loader.dataset),
-#                 100. * batch_idx / len(train_loader), loss.data[0]))
-
 
 train_set = DigitData('input/train.csv')
 train_set = DataLoader(train_set, batch_size = 64, shuffle = True, num_workers = 4)
-# print(train_set)
 test_set = TestData('input/test.csv')
 test_set = DataLoader(test_set, batch_size = 1, shuffle = False, num_workers = 4)
-# train_set.show_img(1)
 
 test_loader = torch.utils.data.DataLoader(
     datasets.MNIST('data', train=False, transform=transforms.Compose([
@@ -117,12 +93,9 @@ test_loader = torch.utils.data.DataLoader(
                    ])),
     batch_size = 64, shuffle = True)
 
-
 cnn_net = CNN()
-# print(cnn_net)
 optimizer = torch.optim.Adam(cnn_net.parameters(), lr = LR)
 loss_func = nn.CrossEntropyLoss()
-
 
 def train(epoch):
     cnn_net.train()
@@ -147,13 +120,8 @@ def pred():
         data = Variable(data, volatile=True)
         output = cnn_net(data)
         pred_single = output.data.max(1, keepdim=True)[1][0][0] # get the index of the max log-probability
-        # print(pred_single)
         pred.append(pred_single)
-        # break
     return pred
-
-
-
 
 def test():
     cnn_net.eval()
@@ -171,35 +139,10 @@ def test():
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
-# for epoch in range(1, EPOCH + 1):
-#     train(epoch)
-#     test()
 cnn_net.load_state_dict(torch.load('mnist_training.pt'))
-image_id = []
-for i in range(1, len(test_set) + 1):
-    image_id.append(i)
-# image_id = [range(1, 10 + 1)]
-# print(image_id)
-test()
+# test()
 prediction = pred()
-
-# print(prediction)
-
-
-raw_data = {'ImageId': image_id,
+raw_data = {'ImageId': range(1, len(test_set) + 1),
         'Label': prediction}
 df = pd.DataFrame(raw_data, columns = ['ImageId', 'Label'])
 df.to_csv('submisiion.csv', index=False)
-
-# raw_data = {'ImageId': [1,2,3], 'Label': [1,2,3]}
-# df = pd.DataFrame(raw_data, columns = ['ImageId', 'Label'])
-# df.to_scv('submisiion.csv')
-# submission.to_csv('submisiion.csv', sep='\t')
-# print(df)
-
-# submission = [['ImageId', 'Label']]
-# submission.append([1,2])
-# df = pd.DataFrame(data = submission)
-# print(df)
-# test()
-# torch.save(cnn_net.state_dict(), 'mnist_training.pt')
